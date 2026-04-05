9d30731c80b4e284de41e8993a92660c8f66cc19
Maydoughrima <johnluismercado863@gmail.com>
2026-04-05 11:04:57 +0800

    fix the url issues on the pages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-import "./styles/float.css"
+import "./styles/float.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Taskora-Demo">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
